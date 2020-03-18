8a42eb8af6ed8f921a584e7028e232fec93c523c
Emmanuel Tweneboah <twentekghana@gmail.com>
2020-03-18 20:50:39 +0800

    Commit

--- src/App.js
+++ src/App.js
@@ -1,63 +1,10 @@
 import React from "react";
-import "./App.css";
-import { ThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Theme from "./config/Theme";
-import NavbarDashboard from "./components/Navbar/NavbarDashboard/NavbarDashboard";
-import ProjectLists from "./components/Projects/ProjectLists";
-import ProjectExpensesList from "./components/Expenses/ProjectExpensesList";
-import ProjectsDashboard from "./components/Projects/ProjectsDashboard";
-import ProjectIncomeList from "./components/Income/ProjectIncomeList";
-import CreateIncomeForm from "./components/Forms/Income/CreateIncomeForm";
-import CreateExpensesForm from "./components/Forms/Expenses/CreateExpensesForm";
-import CreateProjectForm from "./components/Forms/Projects/CreateProjectForm";
 
 const App = () => {
   return (
-    <ThemeProvider theme={Theme}>
-      <BrowserRouter>
-        <NavbarDashboard />
-        <Switch>
-          <Route exact path="/projects" component={ProjectLists} />
-
-          <Route
-            exact
-            path="/project/expenses/:projectId"
-            component={ProjectExpensesList}
-          />
-
-          <Route
-            exact
-            path="/project/income/:projectId"
-            component={ProjectIncomeList}
-          />
-
-          <Route
-            exact
-            path="/project/dashboard/:projectId"
-            component={ProjectsDashboard}
-          />
-
-          <Route
-            exact
-            path="/projects/create-project"
-            component={CreateProjectForm}
-          />
-
-          <Route
-            exact
-            path="/projects/create-income/:projectId"
-            component={CreateIncomeForm}
-          />
-
-          <Route
-            exact
-            path="/projects/create-expense/:projectId"
-            component={CreateExpensesForm}
-          />
-        </Switch>
-      </BrowserRouter>
-    </ThemeProvider>
+    <div>
+      <h1>TekLinco</h1>
+    </div>
   );
 };
 
--- src/config/URLs.js
+++
@@ -1,2 +0,0 @@
-export const API_URL =
-  "http://localhost:1337" || "https:teklinco-project-manager.herokuapp.com";
