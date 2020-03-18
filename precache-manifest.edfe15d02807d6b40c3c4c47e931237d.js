5b771f6ac9c099869a5b99b9842dd71094c616f7
Emmanuel Tweneboah <twentekghana@gmail.com>
2020-03-18 16:29:28 +0800

    Updates

---
+++ precache-manifest.edfe15d02807d6b40c3c4c47e931237d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b246849916359c9cc89464cf92a7aec",
+    "url": "/tweneboah/teklinco-project-manager-frontend/index.html"
+  },
+  {
+    "revision": "45760c46b479f32a63eb",
+    "url": "/tweneboah/teklinco-project-manager-frontend/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "601a926e026f201c47f0",
+    "url": "/tweneboah/teklinco-project-manager-frontend/static/css/main.c24bcba1.chunk.css"
+  },
+  {
+    "revision": "45760c46b479f32a63eb",
+    "url": "/tweneboah/teklinco-project-manager-frontend/static/js/2.5d3a7469.chunk.js"
+  },
+  {
+    "revision": "66c97e93764e553b45064883d863f360",
+    "url": "/tweneboah/teklinco-project-manager-frontend/static/js/2.5d3a7469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "601a926e026f201c47f0",
+    "url": "/tweneboah/teklinco-project-manager-frontend/static/js/main.54754cc3.chunk.js"
+  },
+  {
+    "revision": "f8bdd65ddb3207ae2a5d",
+    "url": "/tweneboah/teklinco-project-manager-frontend/static/js/runtime-main.2ad1c2de.js"
+  }
+]);
